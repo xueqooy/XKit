aade2f2166dc42b8807cb394e14b002f68e1e36b
xuejianhui <106154926+xue-nd@users.noreply.github.com>
2023-09-12 20:19:00 +0800

    [feature] add PagingDataManager

---
+++ EDKit/Classes/Error.swift
@@ -0,0 +1,51 @@
+//
+//  Error+isCancelled.swift
+//  EDKit
+//
+//  Created by xueqooy on 2023/9/12.
+//
+
+import Foundation
+
+public extension Error {
+    var isCancelled: Bool {
+        do {
+            throw self
+        } catch URLError.cancelled {
+            return true
+        } catch CocoaError.userCancelled {
+            return true
+        } catch {
+            return false
+        }
+    }
+}
+
+/**
+ Example:
+ 
+ ```
+ enum ONBError: Error, UnderlyingErrorProviding {
+     case notLoggedIn
+     case unrecognizedStep
+     case notDisplayOnboarding
+     case unknown(underlying: Error)
+     
+     var underlyingError: Error? {
+         if case .unknown(let underlying) = self {
+             return underlying
+         }
+         return nil
+     }
+ }
+ ```
+ */
+public protocol UnderlyingErrorProviding {
+    var underlyingError: Error? { get }
+}
+
+extension NSError: UnderlyingErrorProviding {
+    public var underlyingError: Error? {
+        userInfo[NSUnderlyingErrorKey] as? Error
+    }
+}
