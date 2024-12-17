bbcd01a3a76ede151e499ff053d629848b3bc590
xuejianhui <106154926+xue-nd@users.noreply.github.com>
2024-12-17 14:51:07 +0800

    add String+SHA1

---
+++ XKit/String+SHA1.swift
@@ -0,0 +1,23 @@
+//
+//  String+SHA1.swift
+//  XKit
+//
+//  Created by xueqooy on 2024/12/11.
+//
+
+import CommonCrypto
+
+extension String {
+    public func sha1Encoded() -> String {
+        let data = self.data(using: String.Encoding.utf8)!
+        var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
+        CC_SHA1([UInt8](data), CC_LONG(data.count), &digest)
+
+        let output = NSMutableString(capacity: Int(CC_SHA1_DIGEST_LENGTH))
+        for byte in digest {
+            output.appendFormat("%02x", byte)
+        }
+        return output as String
+    }
+}
+
